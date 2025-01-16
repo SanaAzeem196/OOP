25e14d1285730a4992a57825d551b559d046a928
SanaAzeem196 <sanaazeem263@gmail.com>
2025-01-16 15:17:00 +0500

    v1

---
+++ Lab/transcript.java
@@ -0,0 +1,22 @@
+public class transcript {
+    public static void main(String[] args) {
+
+        System.out.println("\t\t\t--------------------");
+        System.out.println("\t\t\tDHA SUFFA UNIVERSITY");
+        System.out.println("\t\t\t--------------------");
+        System.out.println("\t\t\t\tTRANSCRIPT");
+        System.out.println("---------------------------------------------");
+        System.out.println("NAME = SANA AZEEM\t\t\tROLL NO: SE231078");
+        System.out.println("---------------------------------------------");
+        System.out.println("SUBJECT\t\t\t\t\t\t\t   MARKS");
+        System.out.println("---------------------------------------------");
+        System.out.println("PROGRAMMING FUNDAMENTALS\t\t\t98");
+        System.out.println("PROGRAMMING FUNDAMENTALS LAB\t\t93");
+        System.out.println("APPLIED PHYSICS\t\t\t\t\t\t84");
+        System.out.println("ENGLISH\t\t\t\t\t\t\t\t93");
+        System.out.println("CALCULUS\t\t\t\t\t\t\t87");
+        System.out.println("---------------------------------------------");
+        System.out.println("PERCENTAGE =\t\t\t\t\t  90.0%");
+        System.out.println("Cgpa =\t\t\t\t\t\t\t   4.0");
+    }
+}
